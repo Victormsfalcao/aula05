f4d27f7435cf4714bcf3050dae64f11f3fd3174d
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-14 17:31:01 -0300

    Exercicio 06: Media 0 a 10 repetido

--- media0a10.py
+++ media0a10.py
@@ -1,11 +1,16 @@
-nota1 = float(input("Insira a primeira nota: "))
-while nota1 < 0 or nota1 >10:
+resposta = "s"
+while resposta == "s":
     nota1 = float(input("Insira a primeira nota: "))
+    while nota1 < 0 or nota1 >10:
+        nota1 = float(input("Insira a primeira nota: "))
 
-nota2 = float(input("Insira a segunda nota: "))
-
-while nota2 < 0 or nota2 >10:
     nota2 = float(input("Insira a segunda nota: "))
 
-media = (nota1 + nota2)/2
-print(media)
+    while nota2 < 0 or nota2 >10:
+        nota2 = float(input("Insira a segunda nota: "))
+
+    media = (nota1 + nota2)/2
+    print(media)
+    resposta = input("Deseja realizar novo calculo? ").lower()
+
+
