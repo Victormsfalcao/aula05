ff243b635c8fd1623607522d4164dc7e6eb0e708
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-14 16:31:59 -0300

    Exercicio 04: Validador Senha

---
+++ validadorSenha.py
@@ -0,0 +1,12 @@
+senhaCorreta = 123456
+tentativas = 1
+mensagem = "Senha Bloqueada"
+
+while tentativas <=3:
+    senha = int(input("Digite a senha: "))
+    if senha == senhaCorreta:
+       mensagem = "Senha correta"
+       break
+    tentativas +=1
+print(mensagem)
+
