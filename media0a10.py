105ca90e2fb6b6c1d1ffa3121079be7a1f47eca4
Victormsfalcao <victorfalcao782@gmail.com>
2025-04-14 17:10:07 -0300

    Exercicio 05: Media 0 a 10

---
+++ media0a10.py
@@ -0,0 +1,11 @@
+nota1 = float(input("Insira a primeira nota: "))
+while nota1 < 0 or nota1 >10:
+    nota1 = float(input("Insira a primeira nota: "))
+
+nota2 = float(input("Insira a segunda nota: "))
+
+while nota2 < 0 or nota2 >10:
+    nota2 = float(input("Insira a segunda nota: "))
+
+media = (nota1 + nota2)/2
+print(media)
